d05f4d3e5034f7b51ac3c8efc377e78955e71398
sandbava <bsandoz.pro@gmail.com>
2026-02-28 16:53:48 +0100

    remove ApiResource attribute to entity User

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -11,7 +11,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[ApiResource]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
